c1ee219a3f15373c9948563e2fb6e741dc7a7c1e
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-02-27 00:40:58 +0100

    Now importing WEB_APP_DEV_PROXY_URL

--- modules/utils.js
+++ modules/utils.js
@@ -1,4 +1,4 @@
-import { IS_LOCAL_WEB_APP } from "./constants";
+import { IS_LOCAL_WEB_APP, WEB_APP_DEV_PROXY_URL } from "./constants";
 
 export const adaptFetchUrl = (url) => {
 	return IS_LOCAL_WEB_APP ? `${WEB_APP_DEV_PROXY_URL}/${url}` : url;
