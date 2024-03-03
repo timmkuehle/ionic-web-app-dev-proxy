5a46a5fa9340492c3ec0c2b837ab4d8a77b0644f
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-03-03 19:01:00 +0100

    Adjusted allowed host for proxy server

--- modules/constants.js
+++ modules/constants.js
@@ -15,6 +15,6 @@ export const WEB_APP_DEV_PROXY_PORT = 8910;
 
 export const WEB_APP_DEV_PROXY_URL = `http://${WEB_APP_DEV_PROXY_HOST}:${WEB_APP_DEV_PROXY_PORT}`;
 
-export const allowedOrigins = [WEB_APP_DEV_PROXY_URL, "http://localhost:5173"];
+export const allowedOrigins = [WEB_APP_DEV_PROXY_URL, "http://localhost:8100"];
 
 export const MAX_REQUEST_BODY_SIZE_MB = 2;
