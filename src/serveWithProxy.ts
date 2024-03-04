b9f24dd9c060c3698fe916330193c5983f31cb48
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-03-04 21:07:44 +0100

    Improved serveWithProxy script

--- src/serveWithProxy.ts
+++ src/serveWithProxy.ts
@@ -1,12 +1,41 @@
-import { exec } from "child_process";
-import { logIonicServeError, logIonicServeStart } from "./modules/logFunctions";
+import path from "path";
+import { spawn, fork } from "child_process";
+import {
+	logIonicServeAddress,
+	logIonicServeStart
+} from "./modules/logFunctions";
+import { getIonicServeAddress } from "./modules/ionicServe";
+import { IONIC_DEV_SERVER_RUNNING_REGEX } from "../modules/constants";
 
 logIonicServeStart();
 
-exec("ionic serve", (err, stdout, stderr) => {
-	if (err) {
-		logIonicServeError(err);
+const ionicServe = spawn("ionic", ["serve", "--no-open"]);
+
+ionicServe.stdout?.on("data", (data) => {
+	const stringifiedData = data.toString();
+
+	if (IONIC_DEV_SERVER_RUNNING_REGEX.test(data)) {
+		const ionicServeAddress = getIonicServeAddress(
+			stringifiedData,
+			ionicServe
+		);
+
+		if (!ionicServeAddress) return;
+
+		logIonicServeAddress(ionicServeAddress);
+
+		const proxyServer = fork("./scripts/startServer.js");
+
+		return;
 	}
 
-	console.log("Guckste da:", stdout);
+	if (/hmr update/.test(stringifiedData)) {
+		console.log(stringifiedData.replace(/^\[vite\] |\n/g, ""));
+
+		return;
+	}
+});
+
+ionicServe.on("exit", () => {
+	console.log("\nShutting down Ionic app development server ...");
 });
