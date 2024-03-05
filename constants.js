8999ea33244743db6c9c36a65ba2c39ed01123a6
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-03-05 18:29:03 +0100

    Preflight now allows predefined origins AND Ionic serve address

--- constants.js
+++ constants.js
@@ -15,7 +15,7 @@ export const WEB_APP_DEV_PROXY_PORT = 8910;
 
 export const WEB_APP_DEV_PROXY_URL = `http://${WEB_APP_DEV_PROXY_HOST}:${WEB_APP_DEV_PROXY_PORT}`;
 
-export const allowedOrigins = [WEB_APP_DEV_PROXY_URL, "http://localhost:8100"];
+export const ALLOWED_ORIGINS = [WEB_APP_DEV_PROXY_URL];
 
 export const MAX_REQUEST_BODY_SIZE_MB = 2;
 
