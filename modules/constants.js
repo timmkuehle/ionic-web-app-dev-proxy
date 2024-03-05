626dd3f08ecbc56c57f7ba1f531c8663fcd8f708
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-03-05 11:33:27 +0100

    Added dev server hmr regex constant

--- modules/constants.js
+++ modules/constants.js
@@ -22,3 +22,5 @@ export const MAX_REQUEST_BODY_SIZE_MB = 2;
 export const IONIC_DEV_SERVER_RUNNING_REGEX = /Development server running!/;
 
 export const IONIC_DEV_SERVER_LOCAL_REGEX = /Local: |\n/g;
+
+export const IONIC_DEV_SERVER_HMR_REGEX = /hmr update/;
