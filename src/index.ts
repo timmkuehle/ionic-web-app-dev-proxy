1048df5cf917c0678d42584f7394fcffb7ca50ef
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-03-11 20:23:26 +0100

    Added check for ionic serve address arg in exec.js

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,38 @@ import { logExecError } from "./modules/logFunctions";
 import { shutdownProxyServer, startProxyServer } from "./modules/proxyServer";
 
 if (process.argv.length <= 2) {
-	logExecError({
-		code: "EMISSARG",
-		message:
-			"Missing argument: ionic-web-app-dev-proxy needs to be provided with a command to execute"
-	});
+	logExecError(
+		{
+			code: "EMISSARG",
+			message:
+				"Missing argument: ionic-web-app-dev-proxy needs to be provided with a command to execute"
+		},
+		"Note: Valid commands are [serveWithProxy] and [startProxyServer]"
+	);
 
 	process.exit(1);
 }
 
 let proxyServer: Server | null;
 switch (process.argv[2]) {
-	case "startProxyServer":
-		proxyServer = startProxyServer("http://localhost:8100");
+	case "startProxyServer": {
+		if (process.argv.length <= 3) {
+			logExecError(
+				{
+					code: "ENOIONADDRARG",
+					message:
+						"Missing argument: [startProxyServer] has to be provided with an Ionic serve address to resolve preflight requests"
+				},
+				"Example: ionic-web-app-dev-proxy [startProxyServer http://localhost:8100]"
+			);
+
+			break;
+		}
+
+		proxyServer = startProxyServer(process.argv[3]);
+
 		break;
+	}
 	default: {
 		logExecError({
 			code: "EINVARG",
