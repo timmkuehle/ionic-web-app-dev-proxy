f8566a8b89921e90372d53e992046cd4c4270daf
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-03-12 18:41:25 +0100

    Added proxy server constant types | Removed IS_LOCAL_WEB_APP type

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,19 @@
 /**
+ * Constant for Ionic web app development proxy hostname
+ */
+export declare const WEB_APP_DEV_PROXY_HOST: string;
+
+/**
+ * Constant for Ionic web app development proxy port
+ */
+export declare const WEB_APP_DEV_PROXY_PORT: number;
+
+/**
+ * Constant for Ionic web app development proxy URL
+ */
+export declare const WEB_APP_DEV_PROXY_URL: string;
+
+/**
  * Prepends Ionic web app dev proxy URL to reroute fetch calls during local development
  *
  * Returns provided URL unaltered if rerouting is not necessary
@@ -7,8 +22,3 @@
  * @returns {string} Adapted Fetch URL
  */
 export declare function adaptFetchUrl(url: string): string;
-
-/**
- * Constant to check whether Ionic web app is running locally
- */
-export declare const IS_LOCAL_WEB_APP: boolean;
